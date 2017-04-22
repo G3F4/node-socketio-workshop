5c4988d3b27bd85c8e6fa3d92a77f90f62658a4d
G3F4 <kamil.grabek@gmail.com>
2017-04-22 15:46:07 +0200

    server-milestone#0-initial solution

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('express')();
+const { resolve } = require('path');
+const http = require('http').Server(app);
+
+const PORT = process.env.PORT || '30001';
+
+app.get('/', (req, res) => res.sendFile(resolve(__dirname, './index.html')));
+http.listen(PORT, () => console.log([`listening on port ${PORT}`]));
