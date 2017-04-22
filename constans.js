b1d85576aeec2f88008fe5d89a185c9f13c5ec7e
G3F4 <kamil.grabek@gmail.com>
2017-04-22 16:36:27 +0200

    constans order and dependencies fixes

--- constans.js
+++ constans.js
@@ -3,12 +3,12 @@ const DEFAULT_ROOM = 'Poczekalnia';
 const EVENTS = {
   CONNECTION: 'connection',
   DISCONNECT: 'disconnect',
+  MESSAGE: 'message',
   ERROR: 'errors',
   USER: 'user',
   USERS: 'users',
   ROOM: 'room',
   ROOMS: 'rooms',
-  MESSAGE: 'message',
   NAME: 'name',
   PM: 'pm',
 };
